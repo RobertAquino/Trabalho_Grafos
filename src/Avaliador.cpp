a9f56f8a51a6523f0bfb76d47bca0b0a8007d38f
RobertAquino <orobertaquino@gmail.com>
2026-04-29 15:54:22 -0300

    Avaliador pronto e comentado

--- src/Avaliador.cpp
+++ src/Avaliador.cpp
@@ -1,22 +1,38 @@
 #include "../Bibliotecas/Estruturas.hpp"
+#include "../Bibliotecas/Parser_Operation.hpp"
+#include "../Bibliotecas/Parser_JobInfo.hpp"
 #include <queue>
 
-void avaliador(int total_operations, string caminho, int n_jobs, int n_maquinas)
+// calcula o máximo entre dois valore
+double max(double a, double b)
 {
-    Instancia instancia;
-    instancia.inicializaEstruturas(caminho, n_maquinas, n_jobs);
+    return (a > b) ? a : b;
+}
+
+// Esta função é responsável por avaliar a heurística escolhida. Ela basicamente utiliza listas
+// de adjacência para descobrir qual é o tempo final de todas as operações(makespan) e também o tempo
+// final de cada job indivídual, isso será muito importante na hora do cálculo das penalidades
+vector<double> avaliador(int total_operations, string caminho,
+                         int n_jobs, int n_maquinas, const Instancia &instancia,
+                         const vector<JobInfo> &lista_job, double &makespan)
+{
+    // Primeiro declaramos as estruturas de dados
+    OperationsParser operationsParser;
+    vector<Operacao> lista_operacoes = operationsParser.inicializaParser(caminho);
+    // Esse vetor será utilizado para representar a quantidade de pendencias que uma operação tem
     vector<int> grau_entrada;
     grau_entrada.resize(total_operations, 0);
+    // A fila conterá todos as operações que não dependem de uma operação anterior ou máquina
     queue<int> fila;
-    vector<int> tempo_fim_job(n_jobs, 0);
-    vector<int> tempo_inicio_job(n_jobs, 0);
-    int duracao;
-    int tempo_total_instancia = 0;
-    int tempo_atual;
-    int fim_atual;
-    vector<bool> visitados;
-    visitados.resize(total_operations, false);
+    // O tempo final da última operação de cada job
+    vector<double> tempo_final_job(n_jobs, 0);
+    // O tempo inicial de cada operação
+    vector<double> tempo_inicio_operacao(total_operations, 0);
+    // O tempo final de cada operação
+    vector<double> tempo_fim_operacao(total_operations, 0);
 
+    // O 'for' passará pelas duas listas de adjância, gerando as dependencias por job anterior
+    // ou máquina, expressa na forma de arestas de entrada
     for (int i = 0; i < total_operations; i++)
     {
         if (instancia.jobAntecessor[i] != -1)
@@ -26,54 +42,135 @@ void avaliador(int total_operations, string caminho, int n_jobs, int n_maquinas)
             grau_entrada[i]++;
     }
 
+    // Esse 'for' colocará na fila todos as operação que estão prontas para serem executadas
     for (int i = 0; i < total_operations; i++)
     {
         if (instancia.jobAntecessor[i] == -1 && instancia.machAntecessor[i] == -1)
+        {
             fila.push(i);
+        }
     }
 
+    // Enquanto houver uma operação a ser executada, somaremos o tempo inicial de cada operação
+    // com sua duração para saber quando uma operação específica terminará. Se esta operação for
+    // a última de seu job, adicionamos o seu tempo no vetor 'tempo_final_job'
     while (!fila.empty())
     {
+        // Tiramos o elemento da fila
         int atual = fila.front();
         fila.pop();
-        visitados[atual] = true;
 
-        duracao = instancia.tempoProcessamento[atual];
+        double inicio_op_atual = 0;
+
+        // Se o operação atual possuir um antecessor, o tempo inicial dele é o tempo final do antecessor
+        if (instancia.jobAntecessor[atual] != -1)
+        {
+            inicio_op_atual = tempo_fim_operacao[instancia.jobAntecessor[atual]];
+        }
+        // Se o operação não possui um antecessor, o tempo inicial dele é o tempo que a primeira
+        // operação do job chega, o 'release_date'.
+        else
+        {
+            inicio_op_atual = lista_job[lista_operacoes[atual].id_job].release_date;
+        }
+
+        double desocupa_maquina_atual = 0;
+
+        // Uma vez que a operação atual pode não depender apenas de uma operação anterior de mesmo job,
+        // mas de uma máquina que fui usada anteriormente por uma operação de outro job,
+        // o tempo inicial é o valor máximo entre o tempo final da operação do mesmo job ou da
+        // operação de job diferente
+        if (instancia.machAntecessor[atual] != -1)
+            desocupa_maquina_atual = tempo_fim_operacao[instancia.machAntecessor[atual]];
+
+        tempo_inicio_operacao[atual] = max(inicio_op_atual, desocupa_maquina_atual);
+
+        // O tempo final da operação atual é a soma do tempo inicial do mesmo + o tempo de processamento
+        //+ o tempo de setup, se o tempo setup existir
+        double duracao = instancia.tempoProcessamento[atual];
 
         if (instancia.machAntecessor[atual] != -1)
-            duracao += instancia.matriz_setup[instancia.operToMach[atual]][instancia.machAntecessor[atual]][atual];
+            duracao += instancia.matriz_setup[instancia.operToMach[atual]]
+                                             [lista_operacoes[instancia.machAntecessor[atual]].id_job][lista_operacoes[atual].id_job];
 
-        fim_atual = duracao + tempo_inicio_job[atual];
-        tempo_fim_job[atual] = fim_atual;
+        double tempo_final = duracao + tempo_inicio_operacao[atual];
+        tempo_fim_operacao[atual] = tempo_final;
 
-        // falta lidar com os tempos de inicial e final de cada job
-
-        int jobSuc = instancia.jobSucessor[atual];
-        int machSuc = instancia.machSucessor[atual];
-
-        if (jobSuc != -1)
+        // Se a operação atual possuir um sucessor e este tiver grau de entrada
+        // 0, coloca ele na fila, senão atribui ao vetor de tempo final o seu valor
+        if (instancia.jobSucessor[atual] != -1)
         {
-        }
-
-        if (machSuc != -1)
-        {
-        }
-
-        tempo_inicio_job[jobSuc] = max if (instancia.jobSucessor[atual] != -1)
             grau_entrada[instancia.jobSucessor[atual]]--;
-
-        if (instancia.machSucessor[atual] != -1)
-            grau_entrada[instancia.machSucessor[atual]]--;
-
-        // falta criar uma forma de conjunto fechado para os jobs
-        for (int i = 0; i < total_operations; i++)
+            if (grau_entrada[instancia.jobSucessor[atual]] == 0)
+            {
+                fila.push(instancia.jobSucessor[atual]);
+            }
+        }
+        else
         {
-            if (grau_entrada[i] == 0 && visitados[i] != true)
-                fila.push(i);
+            tempo_final_job[lista_operacoes[atual].id_job] = tempo_final;
+        }
+
+        // O mesmo if de cima, porém para a lista de operações em cada máquina
+        if (instancia.machSucessor[atual] != -1)
+        {
+            grau_entrada[instancia.machSucessor[atual]]--;
+            if (grau_entrada[instancia.machSucessor[atual]] == 0)
+            {
+                fila.push(instancia.machSucessor[atual]);
+            }
         }
     }
+    // Calcula o makespan
+    makespan = 0;
+
+    for (int i = 0; i < n_jobs; i++)
+    {
+        if (makespan < tempo_final_job[i])
+            makespan = tempo_final_job[i];
+    }
+    return tempo_final_job;
 }
-int max(int a, int b)
+
+// Essa função chama a função de avaliador e como base no tempo final de cada job calcula as
+// suas penalidades
+vector<double> calcula_custo_total(string caminho, int n_jobs, int n_maquinas, double &makespan)
 {
-    return (a > b) ? a : b;
+    // Declara as estruturas de dados
+    vector<double> tempo_final_job;
+    // Essa estrutura ao final da função terá o valor final de cada job, com suas multas
+    vector<double> tempo_multas_job(n_jobs, 0);
+    Instancia instancia;
+    JobParser jobParser;
+    vector<JobInfo> lista_job = jobParser.inicializaParser(caminho);
+    instancia.inicializaEstruturas(caminho, n_maquinas, n_jobs);
+    tempo_final_job = avaliador((n_jobs * n_maquinas), caminho, n_jobs, n_maquinas, instancia, lista_job, makespan);
+
+    // Esse 'for' passará pelo tempo de cada job e calculará o tempo real com penalidades, através
+    // diferença do tempo total para o tempo esperado
+    for (int i = 0; i < n_jobs; i++)
+    {
+        double job_atual = tempo_final_job[i];
+        // Se não houver penalidade, o valor permanece o mesmo
+        if (job_atual == lista_job[i].due_date)
+            tempo_multas_job[i] = tempo_final_job[i];
+
+        double diferença = lista_job[i].due_date - tempo_final_job[i];
+
+        // Se houver atraso, calculamos a multa em relação o periodo de atraso
+        if (diferença < 0)
+        {
+            double temp = (diferença * lista_job[i].tardiness_penalty) * (-1);
+            tempo_multas_job[i] = tempo_final_job[i] + temp;
+        }
+
+        // Se houver adiamento, calculamos a multa em relação o periodo de adiamento
+        else
+        {
+            double temp = diferença * lista_job[i].earliness_penalty;
+            tempo_multas_job[i] = tempo_final_job[i] + temp;
+        }
+    }
+
+    return tempo_multas_job;
 }
